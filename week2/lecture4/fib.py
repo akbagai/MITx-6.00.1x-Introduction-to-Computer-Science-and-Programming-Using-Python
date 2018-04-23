e0d04aef657c74136e488a2c2decdd9024ebba2c
akbagai <beocon@gmail.com>
2018-04-22 20:58:35 -0500

    update

--- week2/lecture4/fib.py
+++ week2/lecture4/fib.py
@@ -1,14 +1,59 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 10 13:06:06 2016
-
-@author: ericgrimson
+Add caching
 """
+import timeit
+from functools import lru_cache
 
+
+#@lru_cache(maxsize=10000)
 def fib(x):
     """assumes x an int >= 0
-       returns Fibonacci of x"""
+        returns Fibonacci of x"""
     if x == 0 or x == 1:
         return 1
     else:
-        return fib(x-1) + fib(x-2)
+        return fib(x - 1) + fib(x - 2)
+
+
+fib_cache = {}
+def fibm(x):
+    # Check cache
+    if x in fib_cache:
+        return fib_cache[x]
+
+    # Compute
+    if x == 0 or x == 1:
+        return 1
+    else:
+        value = fibm(x - 1) + fibm(x - 2)
+        fib_cache[x] = value
+        return value
+
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+
+    return wrapped
+
+wrapped = wrapper(fibm, 30)
+
+
+# for n in range(1, 10001):
+#    print(n, ":", fib(n))
+
+
+# create timer object
+
+# t1 = timeit.Timer("fibm(30)")
+# n = 5
+# secs = t1.timeit(number=n)
+# print(secs)
+# print(timeit.timeit('1+3', number=100))
+# print(timeit.timeit("fibm(30)", number=100))
+
+#n = 40
+# time = timeit.timeit('fib(1000)', setup='from __main__ import fib, n, fib_cache', number=n)
+#time = timeit.timeit('fib(1000)', setup='from __main__ import fib, n, fib_cache', number=n)
+print(timeit.timeit(wrapped, number=5))
--- week2/lecture4/iterPower.py
+++ week2/lecture4/iterPower.py
@@ -12,4 +12,35 @@ def iter_power(base, exp):
     return total
 
 
-print(iter_power(7.83, 5))
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+    returns: int or float, base^exp
+    '''
+    negative = False
+    if base < 0 and (exp % 2 == 1):
+        negative = True
+
+    base = abs(base)
+    result = 1
+
+    if exp <= 0:
+        return 1
+    elif exp == 1:
+        result = base
+    else:
+        for i in range(exp):
+            result = result * base
+
+    if negative:
+        result = -1 * result
+
+    return result
+
+
+print(iter_power(-7.83, 5))
+print(iterPower(-7.83, 5))
+
+
+
