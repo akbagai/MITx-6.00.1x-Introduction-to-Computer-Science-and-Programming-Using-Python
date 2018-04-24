d8dc2c5b82f54ac8d19e007164f418804533cfad
akbagai <beocon@gmail.com>
2018-04-24 00:21:10 -0500

    Add memoization and timeit

--- week2/lecture4/fib.py
+++ week2/lecture4/fib.py
@@ -3,10 +3,24 @@
 Add caching
 """
 import timeit
+from timeit import Timer
 from functools import lru_cache
 
 
-#@lru_cache(maxsize=10000)
+def fibi(n):
+    """
+    Iterative Fibonacci
+    :param n: nth number to calculate the Fibonacci value
+    :return: Fibonacci value
+    """
+
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+
+@lru_cache(maxsize=10000)
 def fib(x):
     """assumes x an int >= 0
         returns Fibonacci of x"""
@@ -17,6 +31,8 @@ def fib(x):
 
 
 fib_cache = {}
+
+
 def fibm(x):
     # Check cache
     if x in fib_cache:
@@ -37,8 +53,8 @@ def wrapper(func, *args, **kwargs):
 
     return wrapped
 
-wrapped = wrapper(fibm, 30)
 
+wrapped = wrapper(fibm, 1000)
 
 # for n in range(1, 10001):
 #    print(n, ":", fib(n))
@@ -53,7 +69,19 @@ wrapped = wrapper(fibm, 30)
 # print(timeit.timeit('1+3', number=100))
 # print(timeit.timeit("fibm(30)", number=100))
 
-#n = 40
+# n = 40
 # time = timeit.timeit('fib(1000)', setup='from __main__ import fib, n, fib_cache', number=n)
-#time = timeit.timeit('fib(1000)', setup='from __main__ import fib, n, fib_cache', number=n)
-print(timeit.timeit(wrapped, number=5))
+# time = timeit.timeit('fib(1000)', setup='from __main__ import fib, n, fib_cache', number=n)
+##print(timeit.timeit(wrapped, number=5))
+
+
+#t1 = Timer("fib(10)", "from __main__ import  fib")
+
+for i in range(1, 10000):
+    s = "fibm(" + str(i) + ")"
+    t1 = Timer(s, "from __main__ import  fibm")
+    time1 = t1.timeit(3)
+    s = "fib(" + str(i) + ")"
+    t2 = Timer(s, "from __main__ import  fib")
+    time2 = t2.timeit(3)
+    print("n=%2d, fib: %8.6f, fibi:  %7.6f, percent: %10.2f" % (i, time1, time2, time1 / time2))
