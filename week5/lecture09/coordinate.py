d398c6246d1b6ff422648ba0264b55999fb292f4
akbagai <beocon@gmail.com>
2018-05-03 23:34:06 -0500

    Instantiation experiment

--- week5/lecture09/coordinate.py
+++ week5/lecture09/coordinate.py
@@ -7,12 +7,22 @@ Created on Tue May 10 08:18:02 2016
 
 
 class Coordinate(object):
+    i = 0
     def __init__(self, x, y):
         self.x = x
         self.y = y
         #print(object)
 
     def distance(self, other):
+        '''
+        Takes a coordinate, return the a distance to that coordinate.
+        :param other:
+        :return: Distance from current instance to coordinate specified by other
+        '''
+        '''
+        :param other: 
+        :return: 
+        '''
         x_diff_sq = (self.x - other.x) ** 2
         y_diff_sq = (self.y - other.y) ** 2
         return (x_diff_sq + y_diff_sq) ** 0.5
@@ -29,6 +39,9 @@ class Coordinate(object):
     def __repr__(self):
         return "Coordinate(" + str(self.x) + ", " + str(self.y) + ")"
 
+    def static_inc():
+        i += 1
+
 
 c = Coordinate(3, 4)
 origin = Coordinate(0, 0)
@@ -38,3 +51,7 @@ origin = Coordinate(0, 0)
 d = Coordinate(5,7)
 print(d.x)
 print(d.__str__())
+print("hello")
+print(Coordinate.distance(c,d))
+print(c.distance(d))
+#c.distance()
