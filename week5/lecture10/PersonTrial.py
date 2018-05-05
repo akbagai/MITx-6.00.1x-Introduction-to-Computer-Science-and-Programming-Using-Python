e8664fa6350c8e8ec7c0b0608491d0609047a830
akbagai <beocon@gmail.com>
2018-05-05 00:23:26 -0500

    Added sort

--- week5/lecture10/PersonTrial.py
+++ week5/lecture10/PersonTrial.py
@@ -6,6 +6,7 @@ Created on Wed May 18 07:51:28 2016
 """
 
 import datetime
+from dateutil.relativedelta import relativedelta
 
 class Person(object):
     def __init__(self, name):
@@ -23,10 +24,12 @@ class Person(object):
         self.birthday = datetime.date(year,month,day)
 
     def getAge(self):
+        rdelta = relativedelta(datetime.date.today(), self.birthday)
         """returns self's current age in days"""
         if self.birthday == None:
             raise ValueError
-        return (datetime.date.today() - self.birthday).days
+        #return (datetime.date.today() - self.birthday).days
+        return rdelta.years
         
     
     def __lt__(self, other):
@@ -46,27 +49,29 @@ class Person(object):
         
 
 # example usage
+def main():
+    p1 = Person('Mark Zuckerberg')
+    p1.setBirthday(5,14,84)
+    p2 = Person('Drew Houston')
+    p2.setBirthday(3,4,83)
+    p3 = Person('Bill Gates')
+    p3.setBirthday(10,28,55)
+    p4 = Person('Andrew Gates')
+    p5 = Person('Steve Wozniak')
 
-p1 = Person('Mark Zuckerberg')
-p1.setBirthday(5,14,84)
-p2 = Person('Drew Houston')
-p2.setBirthday(3,4,83)
-p3 = Person('Bill Gates')
-p3.setBirthday(10,28,55)
-p4 = Person('Andrew Gates')
-p5 = Person('Steve Wozniak')
 
+    personList = [p1, p2, p3, p4, p5]
 
-personList = [p1, p2, p3, p4, p5]
+    for e in personList:
+        print(e)
 
-for e in personList:
-    print(e)
-    
-personList.sort()
+    personList.sort()
 
-print()
+    print()
 
-for e in personList:
-    print(e)
-    
+    for e in personList:
+        print(e)
+
+if __name__ == '__main__':
+    main()
 
